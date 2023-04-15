06b2b80e01b381b3040f8e73fc6d6e0935c71fd2
Jayesh Jadhav <96299946+Jayesh-Jadhav05@users.noreply.github.com>
2023-04-15 20:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
 
-
+    const k = document.querySelectorAll("strong");
+	k.forEach((elem) =>{
+		elem.style.color = "green";
+	})
 }
 
 
 function return_normal() {
     //Write your code here
 
+	const k = document.querySelectorAll("strong");
+	k.forEach((elem) =>{
+		elem.style.color = "black";
+	})
     
 }
